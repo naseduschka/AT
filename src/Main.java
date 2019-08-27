5c59ea0b113f3aacb0bbc9fab87f286d69ec0da1
onasedki <Olga.Nasedkina@t-systems.com>
2019-08-27 09:10:44 +0300

    initialized a project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("A simple window");
+        jFrame.setSize(400, 300);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+    }
+}
