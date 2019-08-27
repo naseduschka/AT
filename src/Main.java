d8fc50de1315f2ebf07663420490c2816340afcb
onasedki <Olga.Nasedkina@t-systems.com>
2019-08-27 09:19:12 +0300

    The button was added

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,11 @@ public class Main {
         JFrame jFrame = new JFrame("A simple window");
         jFrame.setSize(400, 300);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Container container = jFrame.getContentPane();
+        container.setLayout(new FlowLayout());
+        JButton jButton = new JButton("Press me ^^");
+        container.add(jButton);
+
         jFrame.setVisible(true);
     }
 }
